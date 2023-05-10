e583cf0420c23db256fc1e64d16c3e4d5fd3175c
Dalal_alsolami <dmohamedalsolami@stu.kau.edo.sa>
2023-05-10 20:45:33 +0300

    Dalal Alsulami

---
+++ src/cpit305_project/PartySuppRental.java
@@ -0,0 +1,15 @@
+
+package cpit305_project;
+
+/**
+ *
+ * @author asus
+ */
+public class PartySuppRental {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
